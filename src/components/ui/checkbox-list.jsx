48ad1c5e35e7fc6d5bc685842075e40f8b104f57
lloydllim <37279932+lloydllim@users.noreply.github.com>
2024-12-11 04:00:05 +0800

    checkbox list getSelectedChoices hook

--- src/components/ui/checkbox-list.jsx
+++ src/components/ui/checkbox-list.jsx
@@ -1,25 +1,41 @@
 import { CheckboxGroup, Fieldset } from "@chakra-ui/react"
 import { Checkbox } from "@/components/ui/checkbox"
+import { useState, useEffect } from "react"
 
-export const CheckboxList = ({ items, header, ...rest }) => {
-  return (
-    <Fieldset.Root {...rest}>
-      <CheckboxGroup
-        name="framework">
+export const CheckboxList = ({ items, header, getSelectedChoices, ...rest }) => {
+	const [checkedItems, setCheckedItems] = useState([]);
 
-        <Fieldset.Legend fontSize="sm" mb="2">
-          {header}
-        </Fieldset.Legend>
-        <Fieldset.Content>
-            {
-                items.map( item =>
-                    <Checkbox
-                        key={item.value}
-                        cursor="pointer"
-                        value={item.value}> {item.label} </Checkbox>)
-            }
-        </Fieldset.Content>
-      </CheckboxGroup>
-    </Fieldset.Root>
-  )
+	useEffect(() => {
+		getSelectedChoices( checkedItems )
+	}, [checkedItems])
+
+	const handleCheckboxChange = (e, value) => {
+		if (e.target.checked) {
+			setCheckedItems((prev) => [...prev, value]);
+		} else {
+			setCheckedItems((prev) => prev.filter((item) => item !== value));
+		}
+	};
+
+	return (
+		<Fieldset.Root {...rest}>
+			<CheckboxGroup
+				name="framework">
+
+				<Fieldset.Legend fontSize="sm" mb="2">
+					{header}
+				</Fieldset.Legend>
+				<Fieldset.Content>
+					{
+						items.map(item =>
+							<Checkbox
+								key={item.value}
+								cursor="pointer"
+								onChange={(e) => handleCheckboxChange(e, item.value)}
+								value={item.value}> {item.label} </Checkbox>)
+					}
+				</Fieldset.Content>
+			</CheckboxGroup>
+		</Fieldset.Root>
+	)
 }
